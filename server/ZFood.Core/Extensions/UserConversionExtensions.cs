136d7804fec13cb91d7280f1c6b7a926a42408a0
Thais Mota <thais.mota@zbra.com>
2018-11-28 11:38:32 -0200

    Added User's classes

---
+++ server/ZFood.Core/Extensions/UserConversionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZFood.Core.Extensions
+{
+    class UserConversionExtensions
+    {
+    }
+}
--- server/ZFood.Persistence.API/IRestaurantRepository.cs
+++ server/ZFood.Persistence.API/IRestaurantRepository.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using ZFood.Persistence.API.Entity;
 
 namespace ZFood.Persistence.API
