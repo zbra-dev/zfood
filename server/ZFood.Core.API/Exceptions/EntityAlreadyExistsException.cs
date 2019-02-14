4b236cdad426df48d6031ec7351a24e2165456d6
Thais Mota <thais.mota@zbra.com>
2019-02-14 13:03:31 -0200

    Small fixes

--- server/ZFood.Core.API/Exceptions/EntityAlreadyExistsException.cs
+++ server/ZFood.Core.API/Exceptions/EntityAlreadyExistsException.cs
@@ -4,18 +4,10 @@ namespace ZFood.Core.API.Exceptions
 {
     public class EntityAlreadyExistsException : ValidationException
     {
-        private readonly Type entityType;
-        private readonly string propertyName;
-        private readonly string propertyValue;
-
         public override string Message { get; }
 
-        public EntityAlreadyExistsException(Type entityType, string propertyName, string propertyValue)
+        public EntityAlreadyExistsException(Type entityType, string propertyName, object propertyValue)
         {
-            this.entityType = entityType;
-            this.propertyName = propertyName;
-            this.propertyValue = propertyValue;
-
             Message = $"{entityType.Name}'s {propertyName} {propertyValue} already exists";
         }
     }
