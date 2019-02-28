8832a1a05886bb579acb629601f88896a4de6cb1
Fillipe Rosini <filliperosini@gmail.com>
2019-02-28 16:04:21 -0300

    Testing bootstrap

---
+++ server/ZFood.Tests/IntegrationTestsWebApplicationFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ZFood.Persistence;
+
+namespace ZFood.Tests
+{
+    public class IntegrationTestsWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Create a new service provider.
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+                services.AddDbContext<ZFoodDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("zfood_tests");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                var provider = services.BuildServiceProvider();
+                using (var scope = provider.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var dbContext = scopedServices.GetRequiredService<ZFoodDbContext>();
+
+                    // Ensure the database is created.
+                    dbContext.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
---
+++ server/ZFood.Tests/RestaurantsControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using ZFood.Web.DTO;
+
+namespace ZFood.Tests
+{
+    public class RestaurantsControllerTests
+            : IClassFixture<IntegrationTestsWebApplicationFactory<Web.Startup>>
+    {
+        private const string Url = "/restaurants";
+
+        private readonly HttpClient client;
+
+        public RestaurantsControllerTests(IntegrationTestsWebApplicationFactory<Web.Startup> factory)
+        {
+            client = factory.CreateClient();
+        }
+
+        [Theory]
+        [InlineData(0, 10)]
+        [InlineData(10, 50)]
+        [InlineData(50, 100)]
+        [InlineData(1000, 1000)]
+        [InlineData(1000, 0)]
+        public async Task TestGetRestaurants(int skip, int take)
+        {
+            var url = $"{Url}?skip={skip}&take={take}";
+            var response = await client.GetAsync(url);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+
+            var page = await response.Content.ReadAsAsync<PageDTO<RestaurantDTO>>();
+            Assert.NotNull(page);
+            Assert.True(page.Items.Count() <= take);
+        }
+
+        [Theory]
+        [InlineData("Name Test 1", "Address Test 1")]
+        [InlineData("Name Test 2", "Address Test 2")]
+        [InlineData("Name Test 3", "Address Test 3")]
+        public async Task PostRestaurant(string name, string address)
+        {
+            var restaurant = new RestaurantDTO
+            {
+                Name = name,
+                Address = address,
+            };
+            var creationResponse = await client.PostAsJsonAsync(Url, restaurant);
+            Assert.Equal(HttpStatusCode.Created, creationResponse.StatusCode);
+            Assert.NotNull(creationResponse.Headers.Location);
+
+            var findResponse = await client.GetAsync(creationResponse.Headers.Location);
+            Assert.Equal(HttpStatusCode.OK, findResponse.StatusCode);
+            Assert.Equal("application/json; charset=utf-8", findResponse.Content.Headers.ContentType.ToString());
+
+            var foundRestaurant = await findResponse.Content.ReadAsAsync<RestaurantDTO>();
+            Assert.NotNull(foundRestaurant);
+            Assert.Equal(name, foundRestaurant.Name);
+            Assert.Equal(address, foundRestaurant.Address);
+        }
+    }
+}
